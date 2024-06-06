9e255364e8fbf8f31df73a3a78a81edf4ceb6184
itamarack <tamaraabells@gmail.com>
2024-06-06 11:37:59 +0100

    added extended dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [
+        react(),
+        tsconfigPaths(),
         laravel({
             input: 'resources/js/app.tsx',
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
-        react(),
     ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './vitest.setup.mjs',
+    },
 });
