66edc1e37eeff46a94b6ddc60de52e4ccd5fa0dc
itamarack <tamaraabells@gmail.com>
2024-06-07 13:46:20 +0100

    completed login screen

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -8,13 +8,14 @@ import {
   Center,
   Paper,
   Text,
+  Stack,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { Head, Link, router, usePage } from '@inertiajs/react';
 import { RequestPayload } from '@inertiajs/core';
-import AuthLayout from '@/Layouts/AuthLayout';
+import { LoginAuthLayout } from '@/Layouts';
 import { Surface } from '@/Components';
-import classes from './css/login.module.css';
+import classes from '~/css/login.module.css';
 
 function Login({ status, canResetPassword }: { status?: string; canResetPassword: boolean }) {
   const { errors } = usePage().props;
@@ -35,72 +36,75 @@ function Login({ status, canResetPassword }: { status?: string; canResetPassword
   const onSubmit = (values: RequestPayload) => {
     router.post(route('login'), values);
     form.setErrors(errors);
-    console.log(errors);
   };
 
   return (
-    <AuthLayout>
+    <LoginAuthLayout>
       <Head title="Log in" />
 
       {status && <div className="mb-4 text-sm font-medium text-green-600">{status}</div>}
 
-      <Title ta="center">Welcome back!</Title>
-      <Text ta="center">Sign in to your account to continue</Text>
+      <Center>
+        <Stack>
+          <Title ta="center">Welcome back :)</Title>
+          <Text ta="center">Sign in to your account to continue</Text>
 
-      <Surface component={Paper} className={classes.card}>
-        <form onSubmit={form.onSubmit(values => onSubmit(values))}>
-          <TextInput
-            label="Email"
-            withAsterisk
-            placeholder="your@email.com"
-            key={form.key('email')}
-            {...form.getInputProps('email')}
-            classNames={{ label: classes.label }}
-          />
-          <PasswordInput
-            withAsterisk
-            label="Password"
-            placeholder="Your password"
-            mt="md"
-            key={form.key('password')}
-            {...form.getInputProps('password')}
-            classNames={{ label: classes.label }}
-          />
-          <Group justify="space-between" mt="lg">
-            <Checkbox
-              label="Remember me"
-              key={form.key('password')}
-              {...form.getInputProps('remember')}
-              classNames={{ label: classes.label }}
-            />
-            {canResetPassword && (
+          <Surface component={Paper} className={classes.card}>
+            <form onSubmit={form.onSubmit(values => onSubmit(values))}>
+              <TextInput
+                label="Email"
+                withAsterisk
+                placeholder="your@email.com"
+                key={form.key('email')}
+                {...form.getInputProps('email')}
+                classNames={{ label: classes.label }}
+              />
+              <PasswordInput
+                withAsterisk
+                label="Password"
+                placeholder="Your password"
+                mt="md"
+                key={form.key('password')}
+                {...form.getInputProps('password')}
+                classNames={{ label: classes.label }}
+              />
+              <Group justify="space-between" mt="lg">
+                <Checkbox
+                  label="Remember me"
+                  key={form.key('password')}
+                  {...form.getInputProps('remember')}
+                  classNames={{ label: classes.label }}
+                />
+                {canResetPassword && (
+                  <Text
+                    component={Link}
+                    href={route('password.request')}
+                    size="sm"
+                    className={classes.link}
+                  >
+                    Forgot password?
+                  </Text>
+                )}
+              </Group>
+              <Button fullWidth mt="xl" type="submit">
+                Sign in
+              </Button>
+            </form>
+            <Center mt="md">
               <Text
+                fz="sm"
+                ta="center"
                 component={Link}
-                href={route('password.request')}
-                size="sm"
+                href={route('register')}
                 className={classes.link}
               >
-                Forgot password?
+                Do not have an account yet? Create account
               </Text>
-            )}
-          </Group>
-          <Button fullWidth mt="xl" type="submit">
-            Sign in
-          </Button>
-        </form>
-        <Center mt="md">
-          <Text
-            fz="sm"
-            ta="center"
-            component={Link}
-            href={route('register')}
-            className={classes.link}
-          >
-            Do not have an account yet? Create account
-          </Text>
-        </Center>
-      </Surface>
-    </AuthLayout>
+            </Center>
+          </Surface>
+        </Stack>
+      </Center>
+    </LoginAuthLayout>
   );
 }
 
