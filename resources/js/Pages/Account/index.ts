123e8d89f96a65e9dd096a873d9b748045d45a3e
itamarack <tamaraabells@gmail.com>
2024-10-02 12:41:47 +0100

    remove React.lazy() from apex chart to resolve unwanted css conflicts

--- resources/js/Pages/Account/index.ts
+++ resources/js/Pages/Account/index.ts
@@ -1,4 +1,5 @@
 export { default as Profile } from "./Profile/Profile";
+export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as List } from "./Users/List";
 export { default as Create } from "./Users/Create";
 export { default as Edit } from "./Users/Edit";
